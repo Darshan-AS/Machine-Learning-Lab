5167a8ddf08d321b547c3c190e14d7d88ad05736
DarshanDon <itisdarshan@gmail.com>
2018-11-11 13:51:15 +0530

    Add feature to display confusion matrix in K-means vs EM

--- K-means vs EM/kmeans_vs_em.py
+++ K-means vs EM/kmeans_vs_em.py
@@ -10,13 +10,21 @@ def main():
 
     k_means = KMeans(n_clusters=3)
     k_means.fit(x_train)
-    k_means_accuracy = metrics.adjusted_rand_score(y_train, k_means.predict(x_train))
+    k_means_y_pred = k_means.predict(x_train)
+    k_means_accuracy = metrics.adjusted_rand_score(y_train, k_means_y_pred)
     print(f'K-means accuracy = {round(k_means_accuracy * 100, 2)}%')
+    print('K-means confusion matrix: ')
+    print(metrics.confusion_matrix(y_train, k_means_y_pred))
+    print()
 
     gaussian_mixture = GaussianMixture(n_components=3)
     gaussian_mixture.fit(x_train)
-    gaussian_mixture_accuracy = metrics.adjusted_rand_score(y_train, gaussian_mixture.predict(x_train))
+    gaussian_mixture_y_pred = gaussian_mixture.predict(x_train)
+    gaussian_mixture_accuracy = metrics.adjusted_rand_score(y_train, gaussian_mixture_y_pred)
     print(f'Gaussian Mixture accuracy = {round(gaussian_mixture_accuracy * 100, 2)}%')
+    print('Gaussian Mixture confusion matrix: ')
+    print(metrics.confusion_matrix(y_train, gaussian_mixture_y_pred))
+    print()
 
     if k_means_accuracy > gaussian_mixture_accuracy:
         print('K-means is better than Gaussian mixture')
