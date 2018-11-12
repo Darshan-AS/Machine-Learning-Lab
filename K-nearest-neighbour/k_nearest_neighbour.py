d3e4b855799c147fec49ff1bd17c14e163c11804
DarshanDon <itisdarshan@gmail.com>
2018-11-12 23:27:09 +0530

    Refactor K-nearest-neighbour

--- K-nearest-neighbour/k_nearest_neighbour.py
+++ K-nearest-neighbour/k_nearest_neighbour.py
@@ -1,12 +1,11 @@
 """
 @author: DON
 """
-import numpy
+import pandas
+from sklearn import metrics
 from sklearn.datasets import load_iris
 from sklearn.model_selection import train_test_split
 from sklearn.neighbors import KNeighborsClassifier
-from sklearn import metrics
-import pandas
 
 
 def main():
@@ -21,19 +20,17 @@ def main():
 
     knn_classifier = KNeighborsClassifier(n_neighbors=3)
     knn_classifier.fit(x_train, y_train)
-    y_predicted = knn_classifier.predict(x_test)
+    y_pred = knn_classifier.predict(x_test)
 
-    print_test_results(x_test, y_test, y_predicted, iris_features, iris_target_names)
+    print_test_results(x_test, y_test, y_pred, iris_features, iris_target_names)
 
 
-def print_test_results(x_test, y_test, y_predicted, features, feature_names):
+def print_test_results(x_test, y_test, y_pred, features, feature_names):
     result = pandas.DataFrame(x_test, columns=features)
-    result['Actual Target'] = numpy.apply_along_axis(
-        lambda y: feature_names[y], 0, y_test)
-    result['Predicted target'] = numpy.apply_along_axis(
-        lambda y: feature_names[y], 0, y_predicted)
+    result['Actual Target'] = [feature_names[i] for i in y_test]
+    result['Predicted target'] = [feature_names[i] for i in y_pred]
     print(result)
-    accuracy = metrics.accuracy_score(y_test, y_predicted)
+    accuracy = metrics.accuracy_score(y_test, y_pred)
     print(f'Accuracy = {round(accuracy * 100, 2)}%')
 
 
