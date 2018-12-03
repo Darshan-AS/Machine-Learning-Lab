70f54ac2770b25710392ce6def1dc41a2ec9663b
DarshanDon <itisdarshan@gmail.com>
2018-12-03 20:24:12 +0530

    Refactor Gaussian Naive Bayes

--- Gaussian Naive Bayes/gaussian_naive_bayes.py
+++ Gaussian Naive Bayes/gaussian_naive_bayes.py
@@ -10,64 +10,50 @@ class GausssianNB:
 
     def __init__(self, priors=None):
         self.__mean_variance = {}
-        self.__priors = {} if priors is None else priors
+        self.__priors = pandas.Series([priors])
 
-    def __calculate_mean_variance(self, x_train, y_train):
-        for _class in y_train.unique():
-            filtered_class = x_train[(y_train == _class)]
+    def fit(self, x_train: pandas.DataFrame, y_train: pandas.Series):
+        self.__priors = y_train.value_counts()
+        self.__priors /= self.__priors.sum()
 
-            m_v = []
-            for i in range(x_train.shape[1]):
-                mean = filtered_class[i].mean()
-                variance = math.pow(filtered_class[i].std(), 2)
-                m_v.append((mean, variance))
+        for _class, x_subset in x_train.groupby(y_train):
+            mean = x_subset.mean()
+            variance = x_subset.var()
+            self.__mean_variance[_class] = (mean, variance)
 
-            self.__mean_variance[_class] = m_v
+    def predict(self, x_test: pandas.DataFrame) -> pandas.Series:
+        return pandas.Series([
+            self.__get_prediction(instance) for index, instance in x_test.iterrows()
+        ])
 
-    def fit(self, x_train, y_train):
-        counts = y_train.value_counts().to_dict()
-        self.__priors = {_class: count / y_train.size for _class, count in counts.items()}
-
-        self.__calculate_mean_variance(x_train, y_train)
-
-    @staticmethod
-    def __calculate_probability(value, mean, variance):
-        exponent = math.exp(-(math.pow(value - mean, 2) / (2 * variance)))
-        return (1 / (math.sqrt(2 * math.pi * variance))) * exponent
-
-    def __get_prediction(self, test_instance):
+    def __get_prediction(self, test_instance: pandas.Series):
         posterior_probabilities = {}
         for _class, prior_probability in self.__priors.items():
             likelihood = 0
-            for i in range(test_instance.size):
+            for attribute, value in test_instance.items():
                 prob = self.__calculate_probability(
-                    test_instance[i],
-                    self.__mean_variance[_class][i][0],
-                    self.__mean_variance[_class][i][1])
+                    value,
+                    self.__mean_variance[_class][0][attribute],
+                    self.__mean_variance[_class][1][attribute])
                 if prob > 0:
                     likelihood += math.log(prob)
             posterior_probabilities[_class] = math.log(prior_probability) + likelihood
 
         return max(posterior_probabilities, key=lambda k: posterior_probabilities[k])
 
-    def predict(self, x_test):
-        y_pred = pandas.Series([])
-        for index, test_instance in x_test.iterrows():
-            best_fit_class = self.__get_prediction(test_instance)
-            y_pred = y_pred.append(pandas.Series([best_fit_class]))
-        return y_pred
+    @staticmethod
+    def __calculate_probability(value: float, mean: float, variance: float) -> float:
+        exponent = math.exp(-(math.pow(value - mean, 2) / (2 * variance)))
+        return (1 / (math.sqrt(2 * math.pi * variance))) * exponent
 
     @staticmethod
-    def accuracy_score(y_true, y_pred):
-        accuracy = 0
-        for test, pred in zip(y_true, y_pred):
-            if test == pred:
-                accuracy += 1
-        return accuracy / y_pred.size
+    def accuracy_score(y_true: pandas.Series, y_pred: pandas.Series) -> float:
+        accuracy = [test == pred for test, pred in zip(y_true, y_pred)]
+        return sum(accuracy) / len(accuracy)
 
 
 def main():
-    diabetes_data_set = pandas.read_csv('diabetes_data.csv', header=None)
+    diabetes_data_set = pandas.read_csv('diabetes_data.csv')
 
     train = diabetes_data_set.sample(frac=0.7)
     test = diabetes_data_set.drop(train.index)
